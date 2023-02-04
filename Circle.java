b68f43fdd5410c9ce5d336eafbd132dfd870a51d
marzhanm <124434547+marzhanm@users.noreply.github.com>
2023-02-04 17:02:30 +0600

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,4 +1,29 @@
 public class Circle {
     private final double radius = 1.0;
-    
+
+    public Circle() {
+
+    }
+
+    public Circle(double radius){
+        radius = this.radius;
+    }
+    public double getRadius(){
+
+    }
+    public void setRadius(double radius){
+        radius = this.radius;
+        return;
+    }
+    public double getArea(){
+
+    }
+    public double getCircumference(){
+        final double pi = Math.PI;
+        double c =2*pi*radius;
+        return c;
+    }
+    public String toString(){
+
+    }
 }
